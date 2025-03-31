7439399d9145460c94ec504269dc72fc7b3c6090
Fraj Yassine Lakhal <flakhal@Yassines-MacBook-Pro.local>
2025-03-31 23:12:27 +0200

    Enable ldap auth

--- smartdorm/settings.py
+++ smartdorm/settings.py
@@ -3,6 +3,9 @@
 import os
 from pathlib import Path
 
+import ldap
+from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 # You must set the SECRET_KEY environment variable before running this project
 SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -54,3 +57,37 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LDAP server configuration
+AUTH_LDAP_SERVER_URI = "ldap://ldap.schollheim.net:389"
+AUTH_LDAP_BIND_DN = "cn=admin,dc=schollheim,dc=net"
+AUTH_LDAP_BIND_PASSWORD = os.environ.get("LDAP_ADMIN_PASSWORD")
+
+# User search configuration
+AUTH_LDAP_USER_SEARCH = LDAPSearch(
+    "dc=schollheim,dc=net",
+    ldap.SCOPE_SUBTREE,
+    "(cn=%(user)s)"
+)
+
+# Group search configuration
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
+    "ou=groups2,dc=schollheim,dc=net",
+    ldap.SCOPE_SUBTREE,
+    "(objectClass=groupOfNames)"
+)
+AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr="cn")
+
+# User attributes mapping
+AUTH_LDAP_USER_ATTR_MAP = {
+    "username": "cn",
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail",
+}
+
+# Authentication backend configuration
+AUTHENTICATION_BACKENDS = [
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
